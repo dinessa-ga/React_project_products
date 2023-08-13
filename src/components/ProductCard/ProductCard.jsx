8ed16990288caea177f8b4ee76d876b9cea4f6fb
dinessa-ga <dinessa.ga@gmail.com>
2023-08-13 17:33:51 -0500

    feat: se muestra ProductDetail y se agrega la propiedad -description- en ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './ProductCardStyles.css';
 
-export const ProductCard = ({id, title, price, image}) => {
+export const ProductCard = ({id, title, price, image, description}) => {
 
     return (
         <>
@@ -13,6 +13,7 @@ export const ProductCard = ({id, title, price, image}) => {
 
          </h2>
          <img className="productImage" src={image} alt="Imagen de producto"/>
+         {description && <p>{description}</p>}
          <p>Price:{price}</p>
           
         </>
--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { ProductCard } from "../ProductCard/ProductCard"
+import axios from "axios"
 
 
 
 export const ProductDetail = () =>{
     let {id}=useParams ()
+    const [product, setProduct] = useState({})
+    useEffect(()=>{
+
+        axios.get(`https://fakestoreapi.com/products/${id}`).then((result)=>{
+            setProduct(result.data)
+        })
+    }, [])
 
     return(
 
         <>
         <h1>Información del producto con el identificador {id}</h1>
+        {product && 
+            <ProductCard
+                id={product.id}
+                title={product.title}
+                image={product.image}
+                price={product.price}
+                description={product.description}
+            />
+        }
+        
         
         </>
     )
