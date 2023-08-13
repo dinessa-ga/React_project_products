043cf7238ed115be11b43581718494ca5313897a
dinessa-ga <dinessa.ga@gmail.com>
2023-08-12 22:21:02 -0500

    feat: creando carpeta componentes e instalación axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { ProductCard } from './components/ProductCard/ProductCard';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts]= useState([])
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Lista de productos </h1>
+     <ProductCard title="producto" price="12"/>
     </div>
   );
 }
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,10 @@
+export const ProductCard = ({title, price}) => {
+
+    return (
+        <div >
+         <a href="#"> {title} </a>
+         <p>Price:{price}</p>
+          
+        </div>
+      )
+}
